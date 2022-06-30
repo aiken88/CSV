c7a744b310325569fe99ba3e2444c6f01857de9e
aiken88 <andres.palacios.garcia@live.com>
2022-06-30 10:34:56 +0200

    Actualizacion de datos 30/06/2022

--- actualizar.bat
+++ actualizar.bat
@@ -2,7 +2,7 @@
 cd %ONEDRIVE%"\ProyectosPython\CSV"
 git status
 git add .
-git commit -m "Actualización de datos"
+git commit -m "Actualizacion de datos "%date%
 git push origin main
 ECHO "fin de la actualizacion a github"
 PAUSE
